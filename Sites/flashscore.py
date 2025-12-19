c860c9af9e26c005465ef1e17e12e5c6148b3b53
copyemechijam <copy.emechijam@gmail.com>
2025-12-19 08:21:33 +0000

    26.12.2025,12:30

--- Sites/flashscore.py
+++ Sites/flashscore.py
@@ -310,7 +310,7 @@ async def run_flashscore_analysis(browser: Browser):
     last_processed_info = get_last_processed_info()
 
     # --- Daily Loop ---
-    for day_offset in range(7):
+    for day_offset in range(10):
         target_date = dt.now(NIGERIA_TZ) + timedelta(days=day_offset)
         target_full = target_date.strftime("%d.%m.%Y")
         
